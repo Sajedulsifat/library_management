0166d6499f555a7f7c4745e028f413f69810121d
Sajedulsifat <mdsazidulislamsifat@gmail.com>
2024-12-23 23:48:41 +0600

    aster

--- db_connection.php
+++ db_connection.php
@@ -6,11 +6,11 @@ $databasename="library_mangement";
 $con=mysqli_connect($server,$username,$password,$databasename);
 if($con)
 {
-    echo "connection successful";
+    echo " ";
 }
 else
 {
-    echo "connection failed" .mysqli_connect_errno();
+    echo " " .mysqli_connect_errno();
 }
 
 ?>
--- sign_up_page.php
+++ sign_up_page.php
@@ -8,15 +8,30 @@
 </head>
 <body>
     <form action="add_information.php" method="POST">
-        <label for="name">Name
+        <label for="name">First name 
             <input type="text" name="name" required>
-        </label>
-        <label for="name">Name
+        </label><br>
+        <label for="name">Last name
             <input type="text" name="name" required>
-        </label>
-        <label for="name">Name
-            <input type="text" name="name" required>
-        </label>
+        </label><br>
+        <label for="email">Email
+            <input type="email" name="email" required>
+        </label><br>
+        <label for="password">Password
+            <input type="password" name="password" required>
+        </label><br>
+        <label for="date of birth">Date of birth
+            <input type="date" name="date of birth" required>
+        </label><br>
+        <label for="Address">Address
+            <input type="text" name="Address" required>
+        </label><br>
+        <label for="Phone number">Phone Number
+            <input type="text" name="Phone number" required>
+        </label><br>
+        <label for="mebership date">membership_date
+            <input type="date" name="membership_date" required>
+        </label><br>
     </form>
    
 </body>
